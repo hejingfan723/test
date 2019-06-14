3e1e1884838dc63c64375fba99e94575dfb2cdce
hejingfan <1024104221@qq.com>
2019-06-14 15:32:48 +0800

    test10

--- eureka-service/src/main/java/com/example/demo/dto/User.java
+++ eureka-service/src/main/java/com/example/demo/dto/User.java
@@ -26,22 +26,6 @@ public class User {
 		this.name = name;
 	}
 
-	public String getAge() {
-		return age;
-	}
-
-	public void setAge(String age) {
-		this.age = age;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
 	
 	
 	
